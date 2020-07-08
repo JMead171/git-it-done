f845bba133039d8aaa5090f541826a2488d1a104
Jeff Mead <jeffdmead171@gmail.com>
2020-07-08 10:26:42 -0400

    handle error logic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,15 +1,15 @@
-var userFormE1 = document.querySelector("#user-form");
-var nameInputE1 = document.querySelector("#username");
-var repoContainerE1 = document.querySelector("#repos-container");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
-    var username = nameInputE1.value.trim();
+    var username = nameInputEl.value.trim();
 
     if (username) {
         getUserRepos(username);
-        nameInputE1.value = "";
+        nameInputEl.value = "";
     } else {
         alert("Please enter a GitHub username");
     }
@@ -35,44 +35,45 @@ var getUserRepos = function(user){
 
 var displayRepos = function(repos, searchTerm) {
     if (repos.length ===0) {
-        repoContainerE1.textContent = "No repositories found.";
+        repoContainerEl.textContent = "No repositories found.";
         return;
     }
-    repoContainerE1.textComtent = "";
+    repoContainerEl.textComtent = "";
     repoSearchTerm.textContent = searchTerm;
     for (var i = 0; i < repos.length; i++) {
         // format repo name
         var repoName = repos[i].owner.login + "/" + repos[i].name;
       
         // create a container for each repo
-        var repoE1 = document.createElement("div");
-        repoE1.classList = "list-item flex-row justify-space-between align-center";
-      
+        var repoEl = document.createElement("a");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+
         // create a span element to hold repository name
-        var titleE1 = document.createElement("span");
-        titleE1.textContent = repoName;
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
       
         // append to container
-        repoE1.appendChild(titleE1);
+        repoEl.appendChild(titleEl);
       
         // create a status element
-        var statusE1 = document.createElement("span");
-        statusE1.classList = "flex-row align-center";
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
 
         // check if current repo has issues or not
         if (repos[i].open_issues_count > 0) {
-            statusE1.innerHTML =
+            statusEl.innerHTML =
             "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
         } else {
-            statusE1.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
         }
 
         // append to container
-        repoE1.appendChild(statusE1);
+        repoEl.appendChild(statusEl);
 
         // append container to the dom
-        repoContainerE1.appendChild(repoE1);
+        repoContainerEl.appendChild(repoEl);
       }
   };
 
-userFormE1.addEventListener("submit", formSubmitHandler);
+userFormEl.addEventListener("submit", formSubmitHandler);
--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,20 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -14,7 +29,7 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request");
+            document.location.replace("./index.html");
         }
     });
   };
@@ -71,5 +86,4 @@ var getRepoIssues = function(repo) {
       }
 };
 
-
-  getRepoIssues("facebook/react");
+  getRepoName();
